27fb675883209e2849cd7ab26cdce7153f4da31b
unklerunkle <115643470+unklerunkle@users.noreply.github.com>
2024-10-14 12:17:01 -0400

    Add files via upload

---
+++ api-fuzzer.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Function to display usage
+show_help(){
+	echo "Usage: $0 <WORDLIST> <SC FILTER> <URL>"
+	echo "   -h, --help    Display this help and exit"
+	echo "the URL parameter should be entered in single quotes"
+	echo
+	echo "This script is a simple API fuzzer to help with parameter fuzzing in WFUZZ"
+}
+
+
+#Check if the help option is provided
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+	show_help
+	exit 0
+fi
+
+#Check if the required arguments are provided
+if [ "$#" -ne 3 ]; then
+	show_help
+	exit 1
+fi
+
+#Assign argument to variables
+WORDLIST="$1"
+SC="$2"
+URL="$3"
+
+#run the WFUZZ command
+wfuzz -c -z file,$WORDLIST --sc $SC $URL
